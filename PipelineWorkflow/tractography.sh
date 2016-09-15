5d7cf23d4c6ab2946931230afe6f908405fbb1f1
dionperd <dionperd@gmail.com>
2016-09-15 11:28:21 +0200

    tractography.sh correction

--- PipelineWorkflow/tractography.sh
+++ PipelineWorkflow/tractography.sh
@@ -5,12 +5,11 @@ pushd $DMR
 #Tractography
 #some more/alternative options?: -backtrack -crop_at_gmwmi -seed_dynamic wm_fod.mif -cutoff 0.06
 opt="-seed_gmwmi ./gmwmi.mif -act ./5tt.mif -unidirectional -maxlength $STRMLNS_MAX_LEN -step $STRMLNS_STEP -nthreads $MRTRIX_THRDS"
-tckgen ./wm_fod.mif ./$STRMLNS_NO.tck -number $STRMLNS_NO $opt
+tckgen ./wm_fod.mif ./$STRMLNS_NO.tck -number $STRMLNS_NO $opt
 
 #SIFT filter
 opt="--act ./5TT.mif -nthreads $MRTRIX_THRDS"
-tcksift ./$STRMLNS_NO.tck ./wm_fod.mif ./$STRMLNS_SIFT_NO.tck -term_number $STRMLNS_SIFT_NO $opt
-
+tcksift ./$STRMLNS_NO.tck ./wm_fod.mif ./$STRMLNS_SIFT_NO.tck -term_number $STRMLNS_SIFT_NO $opt
 
 #Visual check (track density image -tdi):
 tckmap ./$STRMLNS_SIFT_NO.tck ./tdi.mif -vox 5 #vox: size of bin
