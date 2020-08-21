d7c730b8715fe86c24b420cc273846b935da4468
punitbatra <batrapunit1999@gmail.com>
2020-08-21 23:49:58 +0530

    adding slider in home page

--- web/src/components/Home/Content/Content.js
+++ web/src/components/Home/Content/Content.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import img1 from '../image/img1.jpeg';
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+import input from './../image/Input.png'
+import configuration from './../image/Configuration.png'
+import job from './../image/JobList.png'
+import tvb from './../image/TVB.jpeg'
+import workflow from './../image/WorkflowList.png'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -14,15 +20,17 @@ const useStyles = makeStyles((theme) => ({
   },
   img:{
     width:'100%',
-    height:'100%'
+    height:'100%',
+    paddingTop:'45px'
   },
   detail:{
-      fontSize:'28px',
-      
+      fontSize:'26px',
+      paddingTop:'40px'
+
       
   }
 }));
-
+const handleOnDragStart = (e) => e.preventDefault()
 export default function CenteredGrid() {
   const classes = useStyles();
 
@@ -30,10 +38,19 @@ export default function CenteredGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item sm={6} md={6} lg={6}>
-          <Paper className={classes.paper}>
-          <img className={classes.img} alt="complex" src={img1} />
-
-          </Paper>
+          <AliceCarousel mouseTrackingEnabled={false}           buttonsDisabled={true}
+    dotsDisabled={true}
+    autoPlayInterval={1000}
+    autoPlayDirection="ltr"
+    autoPlay={true}
+    fadeOutAnimation={true}>
+      <img src={tvb} alt={tvb} onDragStart={handleOnDragStart} className={classes.img} />
+      <img src={input} alt={tvb} onDragStart={handleOnDragStart} className={classes.img} />
+      <img src={configuration} alt={tvb} onDragStart={handleOnDragStart} className={classes.img} />
+      <img src={workflow} alt={tvb} onDragStart={handleOnDragStart} className={classes.img} />
+      <img src={job} alt={tvb} onDragStart={handleOnDragStart} className={classes.img} />
+      
+    </AliceCarousel>
         </Grid>
         <Grid item sm={6} md={6} lg={6}>
           <Paper className={classes.paper}><p className={classes.detail}>A real brain can turn to a virtual brain in 3 steps. First, the patient goes to a RMN scanner, then, the measured images go through a reconstruction pipeline, 
@@ -42,8 +59,7 @@ export default function CenteredGrid() {
            it takes RMN images, processes them and extracts the brain’s connectivity, 
            geometry, parcellations and other data necessary for virtualization. It produces 
            files that are compatible with TVB.<br/> So, Let's Try by uploading the input files!!!</p> </Paper>
-        </Grid>
-        
+        </Grid>   
       </Grid>
     </div>
   );
